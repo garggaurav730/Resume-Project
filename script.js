4dae6c84cf6cf7321d8f6472fe09569bb12cdfbc
Gaurav Garg <garggaurav730@gmail.com>
2021-05-23 17:33:56 +0530

    resume project

--- script.js
+++
@@ -1,8 +0,0 @@
-var navMenuAnchorTags=document.querySelectorAll('navbar-lists a');
-console.log(navMenuAnchorTags);
-
-for(var i=0;i<navMenuAnchorTags.length;i++) {
-  navMenuAnchorTags[i].addEventListener('click',function(event) {
-    // event.preventDefault();
-  });
-}
